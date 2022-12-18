0bb717b6c5efdea97839c2b95b564eb8d963508e
Šimon Líman <vrt2@seznam.cz>
2022-12-18 02:47:19 +0100

    bump

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # yt_music_downloader
+
+Reads bookmark folder with `BOOKMARKS_NAME` and downloads all youtube songs from it. Downloads lists from when provided. 
+
+add .env file with the following content
+
+```
+BOOKMARKS_PATH="C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2\\Bookmarks"
+DOWNLOADS_PATH="downloads"
+BOOKMARKS_NAME="Songs"
+```
---
+++ downloader.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+# ! python3
+
+from __future__ import annotations
+
+import json
+import youtube_dl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+bookmarks_path = os.environ.get("BOOKMARKS_PATH")
+downloads_path = os.environ.get("DOWNLOADS_PATH")
+bookmarks_name = os.environ.get("BOOKMARKS_NAME")
+
+def download_audio(url: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'{downloads_path}/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+
+if __name__ == '__main__':
+
+    with open(bookmarks_path, "r") as f:
+        bookmarks = f.read()
+        json_data = json.loads(bookmarks)
+        for item in json_data["roots"]["bookmark_bar"]["children"]:
+            if item["type"] == "folder" and item["name"] == bookmarks_name:
+                for child in item["children"]:
+                    if child["type"] == "url":
+                        download_audio(child["url"])
